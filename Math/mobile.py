399c04d3a9d391a209756a6cfd28561e6029e2ba
Ashish Arora <ashisharora88888@gmail.com>
2022-03-18 13:20:54 +0530

    Update mobile.py

--- Math/mobile.py
+++ Math/mobile.py
@@ -1,19 +1,34 @@
 class Solution:
-    # @param A : list of integers
-    # @param B : list of integers
-    # @return a list of integers
+
+    def binary_search(self, arr, low, high, x):
+        if high >= low:
+            mid = (high + low) // 2
+            if arr[mid] == x:
+                return (mid+1)
+            elif arr[mid] < x and arr[mid+1] > x:
+                return (mid+1)
+            elif arr[mid] > x:
+                return self.binary_search(arr, low, mid - 1, x) 
+            else:
+                return self.binary_search(arr, mid + 1, high, x)
+        else:
+            return -1
+            
     def solve(self, A, B):
+        C = []
         output = []
         total = 0
-        count = 0
+        for price in A:
+            total = total + price
+            C.append(total)
+        
         for budget in B:
-            for price in A:
-                if (total + price) < budget:
-                    total = total + price
-                    count = count + 1
-                else:
-                    break
-            output.append(count)
-            total = 0
-            count = 0
+            if C[len(C)-1] < budget:
+                output.append(len(C))
+            elif C[0] > budget:
+                output.append(0)
+            else:
+                output.append(self.binary_search(C, 0, len(C)-1, budget))
         return output
+    
+ 
